d9da072a347d64064f2a6df2292e5329896261d8
JBoris <jonathan-boris.ouedraogo@ensg.eu>
2023-01-11 15:00:18 +0100

    first modify

--- src/main/java/org/acme/geometry/Coordinate.java
+++ src/main/java/org/acme/geometry/Coordinate.java
@@ -1,5 +1,22 @@
 package org.acme.geometry;
 
 public class Coordinate {
+    private double x;
+    private double y;
+    public Coordinate(){
+        x = 0.0;
+        y = 0.0;
+    }
+    public Coordinate(double _x,double _y){
+        x = _x;
+        y = _y;
+    }
 
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
 }
